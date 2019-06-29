d2afb689c79a93829c9d87e2edb7dc22cd183f77
Don Ward <10is0420@gmail.com>
2019-06-29 15:19:11 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -19,7 +19,7 @@ def add_element_to_start_of_array(array, element)
 end
 
 def remove_element_from_end_of_array(array)
-  array.slice
+  array.pop
 end
 
 def remove_element_from_start_of_array(array)
